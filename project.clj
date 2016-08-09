240ea07515ae8f58aaea4ade25027327003de941
Achint Sandhu <sandhu@7theta.com>
2016-08-09 19:40:28 -0400

    Upgraded dependencies

--- project.clj
+++ project.clj
@@ -8,17 +8,17 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/idem "0.1.1"
+(defproject com.7theta/idem "0.1.2"
   :description "Library for working with IP Addresses"
   :url "https://github.com/7theta/idem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.7theta/utilis "0.6.1"]
+                 [com.7theta/utilis "0.8.1"]
                  [com.maxmind.geoip2/geoip2 "2.7.0"]
                  [com.stuartsierra/component "0.3.1"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[reloaded.repl "0.2.1"]
+                   :dependencies [[reloaded.repl "0.2.2"]
                                   [org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}}
   :scm {:name "git"
